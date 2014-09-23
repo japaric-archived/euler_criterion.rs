2c6746bb274a51042f4449b300f370fcabdf5d89
Jorge Aparicio <japaric@linux.com>
2014-09-23 03:31:05 -0500

    020: Solve in Haskell, Python and Rust

---
+++ problems/020/020.rs
@@ -0,0 +1,38 @@
+extern crate num;
+extern crate test;
+extern crate time;
+
+use num::bigint::ToBigUint;
+use std::io::stdio;
+use std::iter::{AdditiveIterator, MultiplicativeIterator};
+use std::os;
+
+fn solution() -> uint {
+    range(2u, 100).
+        map(|x| x.to_biguint().unwrap()).
+        product().
+        to_string().
+        as_slice().
+        chars().
+        filter_map(|c| c.to_digit(10)).
+        sum()
+}
+
+fn main() {
+    match os::args()[] {
+        [_, ref flag] if flag.as_slice() == "-a" => return println!("{}", solution()),
+        _ => {},
+    }
+
+    for line in stdio::stdin().lines() {
+        let iters: u64 = from_str(line.unwrap().as_slice().trim()).unwrap();
+
+        let start = time::precise_time_ns();
+        for _ in range(0, iters) {
+            test::black_box(solution());
+        }
+        let end = time::precise_time_ns();
+
+        println!("{}", end - start);
+    }
+}
---
+++ problems/020/README.md
@@ -0,0 +1,6 @@
+# [Factorial digit sum][0]
+
+![Estimates of the medians][1]
+
+[0]: https://projecteuler.net/problem=20
+[1]: https://rawgit.com/japaric/euler_criterion.rs/master/plots/020.svg
