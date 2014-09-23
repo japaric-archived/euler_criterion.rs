f216a9567c59a94cd401ba1993355c7f37b671ff
Jorge Aparicio <japaric@linux.com>
2014-09-23 02:52:38 -0500

    016: Solve in Haskell, Python and Rust

---
+++ problems/016/016.rs
@@ -0,0 +1,36 @@
+extern crate num;
+extern crate test;
+extern crate time;
+
+use num::bigint::ToBigUint;
+use std::io::stdio;
+use std::iter::AdditiveIterator;
+use std::{char, os};
+
+fn solution() -> uint {
+    std::num::pow(2u.to_biguint().unwrap(), 1000).
+        to_string().
+        as_slice().
+        chars().
+        filter_map(|c| char::to_digit(c, 10)).
+        sum()
+}
+
+fn main() {
+    match os::args()[] {
+        [_, ref flag] if flag.as_slice() == "-a" => return println!("{}", solution()),
+        _ => {},
+    }
+
+    for line in stdio::stdin().lines() {
+        let iters: u64 = from_str(line.unwrap().as_slice().trim()).unwrap();
+
+        let start = time::precise_time_ns();
+        for _ in range(0, iters) {
+            test::black_box(solution());
+        }
+        let end = time::precise_time_ns();
+
+        println!("{}", end - start);
+    }
+}
---
+++ problems/016/README.md
@@ -0,0 +1,6 @@
+# [Power digit sum][0]
+
+![Estimates of the medians][1]
+
+[0]: https://projecteuler.net/problem=16
+[1]: https://rawgit.com/japaric/euler_criterion.rs/master/plots/016.svg
