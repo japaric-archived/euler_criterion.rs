3e26c7fc30d9650766cd82382e71f72eb8e51676
~ <karimellaisy@hotmail.com>
2015-01-18 15:10:59 -0500

    Update and rename 005.rs.broken to 005.rs

---
+++ problems/005/005.rs
@@ -0,0 +1,20 @@
+const BOUND: u64 = 20;
+fn main() {
+    let mut value = 1;
+
+    for f in (2..BOUND+1) {
+        value = lcm(value, f);
+    }
+    println!("{}", value);
+}
+
+#[inline]
+fn lcm(a: u64, b: u64) -> u64 {
+    (a * b) / gcd(a, b)
+}
+
+#[inline]
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 { a }
+    else { gcd(b, a % b) }
+}
